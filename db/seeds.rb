64a064318ad955a260833819876a1edfcd8d82ef
Brian Wangila <bwanjalah@gmail.com>
2022-10-12 17:46:18 +0300

    add data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+service1 = Service.create(service: "Interior Decor", location: "Kinjo Court", address: "Lenana Avenue, Off Abc Road", contact_info: "+254702123453", description: Faker::Lorem.sentence(word_count: rand(10..30)))
+service2 = Service.create(service: "Exterior Painting", location: "ABC Place", address: "Waiyaki way, Opp Safaricom", contact_info: "+254702123453", description: Faker::Lorem.sentence(word_count: rand(10..30)))
+service3 = Service.create(service: "Plumbing Work", location: "Muthangari Duplex", address: "Off Waiyaki way, Opp Safaricom", contact_info: "+254702123453", description: Faker::Lorem.sentence(word_count: rand(10..30)))
+service4 = Service.create(service: "Electrical Installation", location: "Riverside Park", address: "Riverside Park, opp Delta Centre", contact_info: "+254702123453", description: Faker::Lorem.sentence(word_count: rand(10..30)))
