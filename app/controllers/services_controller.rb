ef8af0b64277da8e4009799ac9616ef01e00854f
Brian Wangila <bwanjalah@gmail.com>
2022-10-12 18:04:25 +0300

    add create, show, delete routes and actions

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,29 @@
 class ServicesController < ApplicationController
+
+  def index
+    services = Service.all
+    if services
+      render json: services, status: :ok
+    else
+      render json: {error: "No services found"}, status: :not_found
+    end
+  end
+
+  def show
+    service = Service.find_by(id: params[:id])
+    if service
+      render json: service, status: :ok
+    else
+      render json: {error: "Service not found"}, status: :not_found
+    end
+  end
+
+  def create
+    service = Service.create(service_params)
+    if service.valid?
+      render json: service, status: :created
+    else
+      render json: [error: "validation errors"], status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
 
-  resources :services, only: [:index]
+  resources :services, only: [:index, :show]
 end
