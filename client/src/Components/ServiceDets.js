aeb1768965082b7a4fe663080386e28932ecd9b9
Brian Wangila <bwanjalah@gmail.com>
2022-10-14 00:56:40 +0300

    map function for services

---
+++ client/src/Components/ServiceDets.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import SERVICE from './ServiceData'
+import { Link } from 'react-router-dom'
+
+
+function ServiceDets() {
+
+
+  return (
+    <>
+      {SERVICE.map((service) => {
+        return (
+          <div className="card card-2" style={{width: 18+"rem"}}>
+            <div>
+              <img src={service.image} />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{service.service}</h5>
+              <h6 className="card-subtitle mb-2 text-muted"><span>Ksh.{service.price}</span> ~ estimate cost</h6>
+              <h6 className="card-subtitle mb-2 text-muted">Takes appr' <span>{service.duration}</span> days</h6>
+              <h6 className="card-subtitle mb-2 text-muted"><span>{service.people}</span> ~ people who have used the service</h6>
+              <p className="card-text">{service.description}</p>
+              <Link className="" to="/more-details" ><span>Read more</span></Link>
+            </div>
+          </div>
+        )
+      })}
+      
+    </>
+  )
+}
+
+export default ServiceDets;
