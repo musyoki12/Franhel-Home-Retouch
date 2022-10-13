974fc2abcb14b71009841672a01ecf7e32a9eaa4
Brian Wangila <bwanjalah@gmail.com>
2022-10-14 00:57:07 +0300

    add service data and style the card

--- client/src/Components/ServiceData.js
+++ client/src/Components/ServiceData.js
@@ -3,7 +3,51 @@
 const SERVICE = [
   {
     "service": "Interior Painting",
-    "description": "lorem "
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 10,
+    "price": 45000,
+    "people": 73,
+    "image": "https://www.house-painting-info.com/wp-content/uploads/2018/02/panorama-1080x675.jpg"
+  },
+  {
+    "service": "Exterior Painting",
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 15,
+    "price": 74000,
+    "people": 67,
+    "image": "https://images.unsplash.com/photo-1574359411659-15573a27fd0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnNvbiUyMHBhaW50aW5nJTIwYSUyMGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60"
+  },
+  {
+    "service": "Plumbing Services",
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 2,
+    "price": 5000,
+    "people": 102,
+    "image": "https://stackheating.com/wp-content/uploads/2022/06/plumber-4.jpeg"
+  },
+  {
+    "service": "Gutter and chimney cleaning",
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 1,
+    "price": 7000,
+    "people": 91,
+    "image": "https://sudell.co.uk/wp-content/uploads/2021/08/Gutter-Cleaning-Services.jpg"
+  },
+  {
+    "service": "Electrical Installation",
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 10,
+    "price": 6000,
+    "people": 105,
+    "image": "https://www.designingbuildings.co.uk/w/images/6/60/Electrician-729240_640.jpg"
+  },
+  {
+    "service": "Floor tiling and varnishing",
+    "description": "lorem Veniam numquam blanditiis explicabo enim aliquid illum repudiandae magni reprehenderit corporis eum voluptate qui id eius dolorum ad consequatur dolorem dolor voluptatem necessitatibus provident debitis esse.",
+    "duration": 3,
+    "price": 15000,
+    "people": 49,
+    "image": "https://www.bona.com/contentassets/c22c36d28d62457782f2c51fb1e17acd/floor-sealer-and-finish-guide-hero.png?preset=meta-og-image"
   }
 ]
 
