d1538d55fdfcb7df9234e62399380e2a14ff09e9
Rose Maina <rosewairimu27@gmail.com>
2022-10-13 23:41:01 +0300

    Updated the Login form

--- client/src/App.js
+++ client/src/App.js
@@ -14,35 +14,35 @@ function App() {
   const Login = details => {
     console.log(details);
     
-    if (details.email == adminUser.email && details.password == adminUser.password)
-      console.log("Logged in");
-       setUser({
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+        console.log("Logged in");
+    
+        setUser({
         name: details.name,
         email: details.email
       })
      
-     else {
-      console.log("Details Do Not Match!");
+    }  else {
+     console.log("Details Do Not Match!");
+     setError("Details Do Not Match!");
     }
   }
 
   const Logout = () => {
-    console.log("Logout");
     setUser({ name: "", email: ""})
   }
 
   return (
     <div className="App">
-      <h1>Application Login Page</h1>  
-        {(user.email != "") ? (
+        {(user.email !== "") ? (
           <div className="welcome">
             <h2>Welcome, <span>{user.name}</span></h2>
-            <button>Logout</button>
+            <button onClick={Logout}>Logout</button>
           </div>
-        ) : (
+         ) : (
           <LoginForm Login={Login} error={error} />
-        )}
+        )} 
     </div>   
    );
-  
+}
 export default App;
