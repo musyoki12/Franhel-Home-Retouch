40e0b88e52d4a09a305f467e7894d40c9d2a733c
Brian Wangila <bwanjalah@gmail.com>
2022-10-14 13:32:43 +0300

    temporarily Comment out App

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -14,7 +14,7 @@ function App() {
   const [error, setError] = useState("");
 
   const Login = details => {
-    console.log(details);
+    console.log(details)
     
     if (details.email === adminUser.email && details.password === adminUser.password) {
         console.log("Logged in");
