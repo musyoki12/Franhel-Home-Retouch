6d9985cc5122a1203b1122660a92ec4a04a199ea
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-10-14 04:28:04 +0300

    add auth routes in the file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # login / signup / logout routes
+  post "/signup", to: "clients#create"
+  get "/me", to: "clients#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
