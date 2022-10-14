7dbf6c1260fa71baa9c5af5f4a069e4cd8e167a9
Musyoki <vincenbryaniel@gmail.com>
2022-10-14 10:54:54 +0300

    added footer

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './Components/Navbar'
+
+
+import ShowRoom from './Components/ShowRoom'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <ShowRoom/>
+      <br/>
+      <Footer/>
+    </div>
+  )
+
+}
+
+export default App
