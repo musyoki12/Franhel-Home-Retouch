e0aa9607535c8cd9b41cf6e68a22e5d4a0cd0998
Brian Wangila <bwanjalah@gmail.com>
2022-10-15 18:25:49 +0300

    add authorization to services

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  
+
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error
+
+  before_action :authorize
+
+
+  private
+
+  def authorize
+    @current_user = Client.find_by(id: session[:client_id])
+    render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end
+
+  def unprocessable_entity_error(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
 end
