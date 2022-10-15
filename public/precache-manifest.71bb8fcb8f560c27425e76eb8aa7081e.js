4326c81c3b54dfc906f2481c98ef6f36d124774c
Brian Wangila <bwanjalah@gmail.com>
2022-10-15 21:38:02 +0300

    create build react-app

---
+++ public/precache-manifest.71bb8fcb8f560c27425e76eb8aa7081e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfcf87fe9a3e3f183668",
+    "url": "/static/css/main.5fa30f80.chunk.css"
+  },
+  {
+    "revision": "cfcf87fe9a3e3f183668",
+    "url": "/static/js/main.4b909373.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "317894b92831cc7d2aad",
+    "url": "/static/js/2.c9cf6b1b.chunk.js"
+  },
+  {
+    "revision": "1f9191a8ac477391422d78f27d64a9d2",
+    "url": "/index.html"
+  }
+];
